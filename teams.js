a37f79fb7d17adb3cececa24b755c268d2a731a7
Jaime Pérez <jaimeperezortegadev@gmail.com>
2020-12-25 06:01:48 +0100

    Establecemos los equipos que van a jugar la fase de playoffs

---
+++ teams.js
@@ -0,0 +1,12 @@
+const winnerTeamsGroupA = ["Germany", "Mexico"];
+const winnerTeamsGroupB = ["Brasil", "South Korea"];
+const winnerTeamsGroupC = ["Argentina", "Uruguay"];
+const winnerTeamsGroupD = ["Spain", "Japan"];
+const winnerTeamsGroupE = ["Italy", "Australia"];
+const winnerTeamsGroupF = ["Nederlands", "Sweden"];
+const winnerTeamsGroupG = ["Russia", "Cameroon"];
+const winnerTeamsGroupH = ["Vatican City", "Namibia"];
+
+const playOffTeams = [...winnerTeamsGroupA, ...winnerTeamsGroupB, ...winnerTeamsGroupC, ...winnerTeamsGroupD, ...winnerTeamsGroupE, ...winnerTeamsGroupF, ...winnerTeamsGroupG, ...winnerTeamsGroupH];
+
+console.log(playOffTeams);
